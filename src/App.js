d6740e1f1262f47848a5f4f6e11b5e5364ebbd7c
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2024-10-09 19:58:35 -0400

    added changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101458336 || Jaskiran Gill || DevOps</h5>
+      <h5>Developed By: 101458336 || Jaskiran Gill || DevOps - COMP3104 </h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
